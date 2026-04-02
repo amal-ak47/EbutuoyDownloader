db103215ac841bfe094db132a5b562040cb914c9
AK <aamaal00777@gmail.com>
2026-04-02 16:10:57 +0400

    Added main screen and navigation from the spalsh screen to the main screen

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
 from splash_screen import SplashScreen
+from main_screen import MainScreen
 
 Builder.load_file('splash_screen.kv')
+Builder.load_file('main_screen.kv')
 
 class EbutuoyDownloaderApp(App):
     def build(self):
-        return SplashScreen()
+        sm = ScreenManager()
+        sm.add_widget(SplashScreen(name='splash'))
+        sm.add_widget(MainScreen(name='main'))
+        return sm
 
     def on_start(self):
-        self.root.animate_text()
-if __name__ == "__main__":
-    EbutuoyDownloaderApp().run()
+        self.root.current = 'splash'
+        self.root.get_screen('splash').animate_text()
+
+EbutuoyDownloaderApp().run()
--- splash_screen.py
+++ splash_screen.py
@@ -1,18 +1,20 @@
-from kivy.metrics import sp
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
 from kivy.animation import Animation
+from kivy.metrics import sp
 from kivy.clock import Clock
 
-class SplashScreen(BoxLayout):
+class SplashScreen(Screen):
     def animate_text(self):
-
-        Clock.schedule_once(self._do_animate, 2)
+        Clock.schedule_once(self._do_animate, 1)
+        Clock.schedule_once(self._go_to_main, 5)
 
     def _do_animate(self, dt):
         pop_text = self.ids.pop_text
-
         anim = (
             Animation(duration=0.1) +
             Animation(opacity=1, width=sp(280), duration=0.6, t='out_bounce')
         )
         anim.start(pop_text)
+
+    def _go_to_main(self, dt):
+        self.manager.current = 'main'
